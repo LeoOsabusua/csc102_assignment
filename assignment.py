0dc8741ca85682d933c84f75667403c6aab82aa3
LeoOsabusua <134233321+LeoOsabusua@users.noreply.github.com>
2023-05-25 07:13:14 -0500

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,14 +1,24 @@
 def password():
-    username = (input("Enter user: "))
+    global confirm_password
+    global username
+    username = (input("Enter username: "))
     pass_word =str(input("Enter password: "))
     confirm_password= str(input("Confirm password: "))
     if pass_word == confirm_password:
         print("Password confirmed")
     else:
         print("Invalid password")
-
-    password()
-
-
-
+        password()
 password()
+
+def login():
+    login_username = (input("Enter username:"))
+    login_password = (input("Enter password:"))
+    if login_password == confirm_password:
+        print("login confirmed")
+    else:
+        print("Invalid login password")
+
+        login()
+
+login()
